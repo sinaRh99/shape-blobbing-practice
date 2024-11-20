af6990a58f499eb4711afc268347bd4d4aa31de4
sina rahimi <sina.1999.rahimi@gamil.com>
2024-11-20 15:16:47 +0330

    gooey effect menu

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="stage">
+      <div className="center">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
